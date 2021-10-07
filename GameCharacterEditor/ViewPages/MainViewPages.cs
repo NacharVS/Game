895f4b69b318585fb0843a1f7d353389369275b2
Гизетдинов Искандер <gizetiskander@gmail.com>
2021-10-07 09:32:43 +0300

    i add Methods and Pages

---
+++ GameCharacterEditor/ViewPages/MainViewPages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameCharacterEditor.ViewPages
+{
+    class MainViewPages : MainPagesFoundation
+    {
+
+    }
+}
