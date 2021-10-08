918fd13cb2075e81d0cb955868e5e1e193a4fbde
Alexsandr Krasnov <Sasha-KR90@bk.ru>
2021-10-08 16:11:50 +0300

    Первые шаги!

---
+++ GameCharacterEditor/Unit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameCharacterEditor
+{
+    class Unit
+    {
+        public double Hp_Unit = 100; /*Здоровье юнита*/
+        public double Mp_Unit = 100; /*Манакост юнита*/
+        protected double strenght = 20; /*Сила юнита*/
+        public double dexterity = 20;   /*Ловкость юнита*/
+        public double intelegence = 20; /*Интеллект юнита */
+        public double constitution = 20; /*Телосложение юнита*/
+        public double Extra = 150;  /*Ограничение по способностям*/
+        public double AttackSpeed; /*Это Атакспид*/
+        public double Phusical_Attack = 20;
+        public double Running_Speed = 20;
+        public double Magic_Attack = 20;
+        public double Magic_Defence = 20;
+        public double Phisical_Defence = 20;
+        public double Woolding_Speed = 20;
+    }
+}
---
+++ GameCharacterEditor/Warrior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameCharacterEditor
+{
+    class Warrior : Unit
+    {
+        public void Properties_Warrior()
+        {
+            strenght = 250;
+            dexterity = 70;
+            constitution = 100;
+            intelegence = 50;
+        }
+        public void Health_Unit()
+        {
+            Hp_Unit += strenght * 5;
+        }
+        public void Attack_Unit()
+        {
+            if ((strenght * 5) > 20)
+            {
+                Phusical_Attack = strenght * 5;
+            }
+            else
+            {
+                Phusical_Attack = Extra;
+            }
+        }
+        public void Agility_Buffs_Dexterity()
+        {
+            AttackSpeed += dexterity * 5;
+            Running_Speed += dexterity * 2;
+            Phusical_Attack += dexterity * 3;
+        }
+        public void Agility_Buffs_Intelegence()
+        {
+            Magic_Attack = intelegence * 10;
+            Mp_Unit = intelegence * 5;
+            Magic_Defence = intelegence * 5;
+        }
+        public void Agility_Buffs_Constitution()
+        {
+            Phisical_Defence += constitution * 5;
+            Hp_Unit += constitution * 10;
+        }
+    }
+    
+}
