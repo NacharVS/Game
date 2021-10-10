681f11f4a9b4659b6529caa5cb9a6a4fc33c750f
булат шакиров <a2009-1@mail.ru>
2021-10-10 16:26:26 +0300

    Add transition of the magician to a new equipment window

--- Geroi/Window1.xaml.cs
+++ Geroi/Window1.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Geroi
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            Window1 wd = new Window1();
-            wd.Show();
-            this.Close();
         }
     }
 }
