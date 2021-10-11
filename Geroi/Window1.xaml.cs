74bd62ae1160a13f7193ecc3f4f47f2ff31b0cc5
булат шакиров <a2009-1@mail.ru>
2021-10-11 18:59:56 +0300

    add to mag characteristics

--- Geroi/Window1.xaml.cs
+++ Geroi/Window1.xaml.cs
@@ -40,5 +40,116 @@ namespace Geroi
             Mouse.OverrideCursor = Cursors.Hand;
         }
 
+        private void Plus1_Click(object sender, RoutedEventArgs e)
+        {
+            int st = Convert.ToInt32(Strenght.Text);
+            st += 1;
+            Strenght.Text = st.ToString();
+            int kl = Convert.ToInt32(Kol.Text);
+            kl -= 1;
+            if (kl == 0)
+            {
+                Kol.Text = kl.ToString();
+                Plus1.Visibility = Visibility.Hidden;
+            }
+            else if (kl > 0)
+            {
+                Minus1.Visibility = Visibility.Visible;
+                Kol.Text = kl.ToString();
+            }
+        }
+
+        private void Minus1_Click(object sender, RoutedEventArgs e)
+        {
+            int st = Convert.ToInt32(Strenght.Text);
+            st -= 1;
+            if (st == 0)
+            {
+                Strenght.Text = st.ToString();
+                Minus1.Visibility = Visibility.Hidden;
+            }
+            else if (st > 0)
+            {
+                Plus1.Visibility = Visibility.Visible;
+                Strenght.Text = st.ToString();
+            }
+            int kl = Convert.ToInt32(Kol.Text);
+            kl += 1;
+            Kol.Text = kl.ToString();
+        }
+        private void Plus2_Click(object sender, RoutedEventArgs e)
+        {
+            int st = Convert.ToInt32(Agility.Text);
+            st += 1;
+            Agility.Text = st.ToString();
+            int kl = Convert.ToInt32(Kol.Text);
+            kl -= 1;
+            if (kl == 0)
+            {
+                Kol.Text = kl.ToString();
+                Plus2.Visibility = Visibility.Hidden;
+            }
+            else if (kl > 0)
+            {
+                Minus2.Visibility = Visibility.Visible;
+                Kol.Text = kl.ToString();
+            }
+        }
+
+        private void Minus2_Click(object sender, RoutedEventArgs e)
+        {
+            int st = Convert.ToInt32(Agility.Text);
+            st -= 1;
+            if (st == 0)
+            {
+                Agility.Text = st.ToString();
+                Minus2.Visibility = Visibility.Hidden;
+            }
+            else if (st > 0)
+            {
+                Plus2.Visibility = Visibility.Visible;
+                Agility.Text = st.ToString();
+            }
+            int kl = Convert.ToInt32(Kol.Text);
+            kl += 1;
+            Kol.Text = kl.ToString();
+        }
+        private void Plus3_Click(object sender, RoutedEventArgs e)
+        {
+            int st = Convert.ToInt32(Intelligence.Text);
+            st += 1;
+            Intelligence.Text = st.ToString();
+            int kl = Convert.ToInt32(Kol.Text);
+            kl -= 1;
+            if (kl == 0)
+            {
+                Kol.Text = kl.ToString();
+                Plus3.Visibility = Visibility.Hidden;
+            }
+            else if (kl > 0)
+            {
+                Minus3.Visibility = Visibility.Visible;
+                Kol.Text = kl.ToString();
+            }
+        }
+
+        private void Minus3_Click(object sender, RoutedEventArgs e)
+        {
+            int st = Convert.ToInt32(Intelligence.Text);
+            st -= 1;
+            if (st == 0)
+            {
+                Intelligence.Text = st.ToString();
+                Minus3.Visibility = Visibility.Hidden;
+            }
+            else if (st > 0)
+            {
+                Plus3.Visibility = Visibility.Visible;
+                Intelligence.Text = st.ToString();
+            }
+            int kl = Convert.ToInt32(Kol.Text);
+            kl += 1;
+            Kol.Text = kl.ToString();
+        }
     }
 }
