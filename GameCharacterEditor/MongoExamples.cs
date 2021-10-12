fc052c3c4be7c82fa6f4cec49c1ba19e393f2c6d
алина алина <fkbyf-2003zv@mail.ru>
2021-10-12 13:07:16 +0300

    StartWork

--- GameCharacterEditor/MongoExamples.cs
+++ GameCharacterEditor/MongoExamples.cs
@@ -4,38 +4,39 @@ using System.Text;
 
 namespace GameCharacterEditor
 {
-    class MongoExamples
-    {
-        class Person
-        {
-            public Person(string name, int age, List<string> items)
-            {
-                this.name = name;
-                this.age = age;
-                this.items.AddRange(items);
-            }
-            public Person(string name, int age, int group)
-            {
-                this.name = name;
-                this.age = age;
-                this.Group = group;
-            }
+//    class MongoExamples
+//    {
+//        class Person
+//        {
+//            public Person(string name, int age, List<string> items)
+//            {
+//                this.name = name;
+//                this.age = age;
+//                this.items.AddRange(items);
+//            }
+//            public Person(string name, int age, int group)
+//            {
+//                this.name = name;
+//                this.age = age;
+//                this.Group = group;
+//            }
 
-            [BsonId]
-            ObjectId _id;
-            [BsonElement("Имя")]
-            public string name { get; set; }
-            public int age { get; set; }
-            [BsonIgnoreIfDefault]
-            public int Group { get; set; }
-            public List<string> items = new List<string>();
+//            [BsonId]
+//            ObjectId _id;
+//            [BsonElement("Имя")]
+//            public string name { get; set; }
+//            public int age { get; set; }
+//            [BsonIgnoreIfDefault]
+//            public int Group { get; set; }
+//            public List<string> items = new List<string>();
 
-            public static void AddToDataBase(Person person)
-            {
-                var client = new MongoClient("mongodb://localhost");
-                var database = client.GetDatabase("321gr2021brr");
-                var collection = database.GetCollection<Person>("qqPerson");
-                collection.InsertOne(person);
-            }
-        }
+//            public static void AddToDataBase(Person person)
+//            {
+//                var client = new MongoClient("mongodb://localhost");
+//                var database = client.GetDatabase("321gr2021brr");
+//                var collection = database.GetCollection<Person>("qqPerson");
+//                collection.InsertOne(person);
+//            }
+//        }
+//    }
 }
