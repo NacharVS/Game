e0ce0e4b33e5d8d359407c18c0ce139d4f099cae
Гизетдинов Искандер <gizetiskander@gmail.com>
2021-10-14 19:49:58 +0300

    I have redone my solution

--- Game/View/MainWindow.xaml.cs
+++ Game/View/MainWindow.xaml.cs
@@ -13,10 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MyGame.View
+namespace Game.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
