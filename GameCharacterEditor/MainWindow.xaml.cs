0fafa8903e04771b822ce3bd38f7762a608063bb
Alexsandr Krasnov <Sasha-KR90@bk.ru>
2021-10-14 11:10:16 +0300

    Add photo

--- GameCharacterEditor/MainWindow.xaml.cs
+++ GameCharacterEditor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,46 @@ namespace GameCharacterEditor
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ChooseHero_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (Hero.SelectedIndex)
+            {
+                case 0:
+                    Choosehero();
+                    break;
+                case 1:
+                    Choosehero();
+                    break;
+                case 2:
+                    Choosehero();
+                    break;
+                case 3:
+                    Choosehero();
+                    break;
+
+            }
+        }
+
+        private void Choosehero()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == true)
+            {
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.UriSource = new Uri(ofd.FileName);
+                image.EndInit();
+                ChooseHero.Source = image;
+            }
+            
         }
     }
 }
