2bd5634bbdb0438508323a3634ad94c2ebbe3fde
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2021-10-18 14:35:03 +0300

    class change

--- GameCharacterEditor/MainWindow.xaml.cs
+++ GameCharacterEditor/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Shapes;
 namespace GameCharacterEditor
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+      /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -30,42 +30,66 @@ namespace GameCharacterEditor
         {
             if (radBtn_Warrior.IsChecked == true)
             {
-                string link = "C:/Users/User/source/repos/Game/GameCharacterEditor/Resourse/knights_m.png";
+                string link = "C:/Users/User/Source/Repos/Game/ver2/GameCharacterEditor/Resourse/knight.jpg";
 
                 using (Stream fs = new FileStream(link, FileMode.Open))
                 {
                     Bitmap bmp = new Bitmap(fs);
                     image_character.Source = bmp.BitmapToImageSource();
                 }
+
+                Warrior war = new Warrior();
+                GetUnitInfo(war);
             }
 
             if (radBtn_Witch.IsChecked == true)
             {
-                string link = "C:/Users/User/source/repos/Characters/Characters/Resourse/knights_m.png";
+                string link = "C:/Users/User/Source/Repos/Game/ver2/GameCharacterEditor/Resourse/witch.png";
 
                 using (Stream fs = new FileStream(link, FileMode.Open))
                 {
                     Bitmap bmp = new Bitmap(fs);
                     image_character.Source = bmp.BitmapToImageSource();
                 }
+
+                Witch witch = new Witch();
+                GetUnitInfo(witch);
             }
 
             if (radBtn_Rogue.IsChecked == true)
             {
-                string link = "C:/Users/User/source/repos/Characters/Characters/Resourse/knights_m.png";
+                string link = "C:/Users/User/Source/Repos/Game/ver2/GameCharacterEditor/Resourse/rogue.jpg";
 
                 using (Stream fs = new FileStream(link, FileMode.Open))
                 {
                     Bitmap bmp = new Bitmap(fs);
                     image_character.Source = bmp.BitmapToImageSource();
                 }
+
+                Rogue rogue = new Rogue();
+                GetUnitInfo(rogue);
             }
 
             if (radBtn_Rogue.IsChecked == false & radBtn_Warrior.IsChecked == false & radBtn_Witch.IsChecked == false)
             {
                 MessageBox.Show("Выберите класс персонажа");
             }
-
+        }
+        private void GetUnitInfo(Unit unit)
+        {
+            textb_name_character.Text = unit.Name;
+            textb_constitution.Text = unit.Constitution.ToString();
+            textb_dexterity.Text = unit.Dexterity.ToString();
+            textb_intelligent.Text = unit.Intelligence.ToString();
+            textb_strenght.Text = unit.Strenght.ToString();
+            textb_pResist.Text = unit.PResist.ToString();
+            textb_mResist.Text = unit.MResist.ToString();
+            textb_hp.Text = unit.Health.ToString();
+            textb_attack_speed.Text = unit.AttackSpeed.ToString();
+            textb_walk_speed.Text = unit.WalkSpeed.ToString();
+            textb_mana.Text = unit.Mana.ToString();
+            textb_attack_magic.Text = unit.MAttack.ToString();
+            textb_attack.Text = unit.PAttack.ToString();
         }
     }
 
--- GameCharacterEditor/Unit.cs
+++ GameCharacterEditor/Unit.cs
@@ -6,22 +6,48 @@ namespace GameCharacterEditor
 {
     public class Unit
     {
-        protected int strenght, dexterity, intelligence, constitution, health, mana, pAttack, mAttack, pResist, mResist, attackSpeed, walkSpeed;
+        protected string name = "";
+        protected int strenght = 0;
+        protected int constitution = 0;
+        protected int intelligence = 0;
+        protected int dexterity = 0;
+        protected int pResist = 0;
+        protected int mResist = 0;
+        protected int health = 0;
+        protected int mana = 0;
+        protected int pAttack = 0;
+        protected int mAttack = 0;
+        protected int walkSpeed = 0;
+        protected int attackSpeed = 0;
 
+        public string Name { get => name; set => name = value; }
+        public int Strenght { get => strenght; set => strenght = value; }
+        public int Constitution { get => constitution; set => constitution = value; }
+        public int Intelligence { get => intelligence; set => intelligence = value; }
+        public int Dexterity { get => dexterity; set => dexterity = value; }
+        public int PResist { get => pResist; set => pResist = value; }
+        public int MResist { get => mResist; set => mResist = value; }
+        public int Health { get => health; set => health = value; }
+        public int Mana { get => mana; set => mana = value; }
+        public int PAttack { get => pAttack; set => pAttack = value; }
+        public int MAttack { get => mAttack; set => mAttack = value; }
+        public int WalkSpeed { get => walkSpeed; set => walkSpeed = value; }
+        public int AttackSpeed { get => attackSpeed; set => attackSpeed = value; }
         public Unit()
         {
+            name = "unit";
             strenght = 20;
             dexterity = 20;
             intelligence = 20;
             constitution = 20;
-            health = 100;
-            mana = 100;
-            pAttack = 20;
-            mAttack = 20;
-            pResist = 20;
-            mResist = 20;
-            attackSpeed = 20;
-            walkSpeed = 20;
+            health = strenght * 5 + constitution * 10;
+            mana = intelligence * 5;
+            pAttack = strenght * 5;
+            mAttack = intelligence * 10;
+            pResist = constitution * 5 + dexterity*3;
+            mResist = intelligence * 5;
+            attackSpeed = dexterity * 5;
+            walkSpeed = dexterity * 2;
         }
     }
 
@@ -29,18 +55,11 @@ namespace GameCharacterEditor
     {
         public Warrior()
         {
-            strenght = 30;
-            dexterity = 20;
-            intelligence = 10;
-            constitution = 25;
-            health = 100;
-            mana = 100;
-            pAttack = 20;
-            mAttack = 20;
-            pResist = 20;
-            mResist = 20;
-            attackSpeed = 20;
-            walkSpeed = 20;
+            name = "War";
+            strenght = 250;
+            dexterity = 70;
+            intelligence = 50;
+            constitution = 100;
         }
     }
 
@@ -48,18 +67,11 @@ namespace GameCharacterEditor
     {
         public Witch()
         {
-            strenght = 10;
-            dexterity = 20;
-            intelligence = 30;
-            constitution = 20;
-            health = 20;
-            mana = 20;
-            pAttack = 20;
-            mAttack = 20;
-            pResist = 20;
-            mResist = 20;
-            attackSpeed = 20;
-            walkSpeed = 20;
+            name = "witch";
+            strenght = 50;
+            dexterity = 50;
+            intelligence = 250;
+            constitution = 50;
         }
     }
 
@@ -67,18 +79,11 @@ namespace GameCharacterEditor
     {
         public Rogue()
         {
-            strenght = 15;
-            dexterity = 30;
-            intelligence = 25;
-            constitution = 15;
-            health = 20;
-            mana = 20;
-            pAttack = 20;
-            mAttack = 20;
-            pResist = 20;
-            mResist = 20;
-            attackSpeed = 20;
-            walkSpeed = 20;
+            name = "rogue";
+            strenght = 60;
+            dexterity = 250;
+            intelligence = 100;
+            constitution = 60;
         }
     }
 }
