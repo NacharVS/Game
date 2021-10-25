a80c593c083b23fa5be30e42cca9f79fe9cd18b6
Анатолий Жмышенко <gggame184@gmail.com>
2021-10-25 17:06:26 +0300

    Started a project

--- GameCharacterEditor/MainWindow.xaml.cs
+++ GameCharacterEditor/MainWindow.xaml.cs
@@ -24,52 +24,5 @@ namespace GameCharacterEditor
         {
             InitializeComponent();
         }
-        void PrintText(object sender, SelectionChangedEventArgs args)
-        {
-            ListBoxItem lbi = ((sender as ListBox).SelectedItem as ListBoxItem);
-            tb.Text = " You selected " + lbi.Content.ToString() + ".";
-        }
-
-        private void btnSave_Click(object sender, RoutedEventArgs e)
-        {
-
-            MessageBox.Show("person created!!!!!!!");
-        }
-
-        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
-        {
-            dam.Text = "200";
-            def.Text = "400";
-            att.Text = "111";
-            spe.Text = "700";
-            mag.Text = "666";
-            hp.Text = "300";
-        }
-
-        private void ListBoxItem_Selected_1(object sender, RoutedEventArgs e)
-        {
-            dam.Text = "2000";
-            def.Text = "4000";
-            att.Text = "1101";
-            spe.Text = "7000";
-            mag.Text = "6606";
-            hp.Text = "3000";
-        }
-
-        private void ListBoxItem_Selected_2(object sender, RoutedEventArgs e)
-        {
-            dam.Text = "20";
-            def.Text = "40";
-            att.Text = "11";
-            spe.Text = "70";
-            mag.Text = "66";
-            hp.Text = "30";
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-        //    Window1 win1 = new Window1();
-        //    win1.Show();
-        }
     }
 }
