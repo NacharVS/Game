8ba088c1ac12f699bc2ea0b0e90ccc09d9298195
Diyar Tesfaye <mr.vasya10.10@gmail.com>
2021-10-26 12:15:49 +0300

    add new extrapoints settings

--- GameCharacterEditor/MainWindow.xaml.cs
+++ GameCharacterEditor/MainWindow.xaml.cs
@@ -32,8 +32,8 @@ namespace GameCharacterEditor
 
         private void ChooseWarrior_Click(object sender, RoutedEventArgs e)
         {
-            PickButton.IsEnabled = PA_down.IsEnabled = PA_up.IsEnabled = MA_down.IsEnabled = MA_up.IsEnabled =
-            PR_down.IsEnabled = PR_up.IsEnabled = MR_down.IsEnabled = MR_up.IsEnabled = true;
+            PickButton.IsEnabled = Strenght_down.IsEnabled = Strenght_up.IsEnabled = Constitution_down.IsEnabled = Constitution_up.IsEnabled =
+            Dexterity_down.IsEnabled = Dexterity_up.IsEnabled = Intelligence_down.IsEnabled = Intelligence_up.IsEnabled = true;
 
             if (ChooseWarriorRB.IsChecked == true)
             {
@@ -44,7 +44,7 @@ namespace GameCharacterEditor
                 MP_TB.Text = warrior.ManaPoint.ToString();
                 Strenght_TB.Text = warrior.Strenght.ToString();
                 Dexterity_TB.Text = warrior.Dexterity.ToString();
-                Intelligenct_TB.Text = warrior.Intelligence.ToString();
+                Intelligence_TB.Text = warrior.Intelligence.ToString();
                 Constitution_TB.Text = warrior.Constitution.ToString();
                 Extra_TB.Text = warrior.Extra.ToString();
                 PA_TB.Text = warrior.PAttack.ToString();
@@ -64,7 +64,7 @@ namespace GameCharacterEditor
                 MP_TB.Text = rogue.ManaPoint.ToString();
                 Strenght_TB.Text = rogue.Strenght.ToString();
                 Dexterity_TB.Text = rogue.Dexterity.ToString();
-                Intelligenct_TB.Text = rogue.Intelligence.ToString();
+                Intelligence_TB.Text = rogue.Intelligence.ToString();
                 Constitution_TB.Text = rogue.Constitution.ToString();
                 Extra_TB.Text = rogue.Extra.ToString();
                 PA_TB.Text = rogue.PAttack.ToString();
@@ -83,7 +83,7 @@ namespace GameCharacterEditor
                 MP_TB.Text = sorcerer.ManaPoint.ToString();
                 Strenght_TB.Text = sorcerer.Strenght.ToString();
                 Dexterity_TB.Text = sorcerer.Dexterity.ToString();
-                Intelligenct_TB.Text = sorcerer.Intelligence.ToString();
+                Intelligence_TB.Text = sorcerer.Intelligence.ToString();
                 Constitution_TB.Text = sorcerer.Constitution.ToString();
                 Extra_TB.Text = sorcerer.Extra.ToString();
                 PA_TB.Text = sorcerer.PAttack.ToString();
@@ -114,12 +114,12 @@ namespace GameCharacterEditor
 
             switch (bttn)
             {
-                case "PA_up":
+                case "Strenght_up":
                     int pa = int.Parse(PA_TB.Text);
                     if(pa < 30)
                     {
                         ++pa;
-                        PA_TB.Text = pa.ToString();
+                        Strenght_TB.Text = pa.ToString();
                     }
                     else
                     {
@@ -127,12 +127,12 @@ namespace GameCharacterEditor
                         Extra_TB.Text = ex.ToString();
                     }
                     break;
-                case "MA_up":
+                case "Constitution_up":
                     int ma = int.Parse(MA_TB.Text);
                     if (ma < 30)
                     {
                         ++ma;
-                        MA_TB.Text = ma.ToString();
+                        Constitution_TB.Text = ma.ToString();
                     }
                     else
                     {
@@ -140,12 +140,12 @@ namespace GameCharacterEditor
                         Extra_TB.Text = ex.ToString();
                     }
                     break;
-                case "PR_up":
+                case "Dexterity_up":
                     int pr = int.Parse(PR_TB.Text);
                     if (pr < 30)
                     {
                         ++pr;
-                        PR_TB.Text = pr.ToString();
+                        Dexterity_TB.Text = pr.ToString();
                     }
                     else 
                     {
@@ -153,12 +153,12 @@ namespace GameCharacterEditor
                         Extra_TB.Text = ex.ToString();
                     }
                     break;
-                case "MR_up":
+                case "Intelligence_up":
                     int mr = int.Parse(MR_TB.Text);
                     if(mr < 30)
                     {
                         ++mr;
-                        MR_TB.Text = mr.ToString();
+                        Intelligence_TB.Text = mr.ToString();
                     }
                     else
                     {
@@ -180,14 +180,14 @@ namespace GameCharacterEditor
 
             switch (bttn)
             {
-                case "PA_down":
+                case "Strenght_down":
                     int pa = int.Parse(PA_TB.Text);
                     if(pa > 20)
                     {
                         --pa;
                         ++ex;
                         Extra_TB.Text = ex.ToString();
-                        PA_TB.Text = pa.ToString();
+                        Strenght_TB.Text = pa.ToString();
                     }
                     else
                     {
@@ -195,14 +195,14 @@ namespace GameCharacterEditor
                         Extra_TB.Text = ex.ToString();
                     }
                     break;
-                case "MA_down":
+                case "Constitution_down":
                     int ma = int.Parse(MA_TB.Text);
                     if(ma > 20)
                     {
                         --ma;
                         ++ex;
                         Extra_TB.Text = ex.ToString();
-                        MA_TB.Text = ma.ToString();
+                        Constitution_TB.Text = ma.ToString();
                     }
                     else 
                     {
@@ -210,14 +210,14 @@ namespace GameCharacterEditor
                         Extra_TB.Text = ex.ToString();
                     }
                     break;
-                case "PR_down":
+                case "Dexterity_down":
                     int pr = int.Parse(PR_TB.Text);
                     if (pr > 20)
                     {
                         --pr;
                         ++ex;
                         Extra_TB.Text = ex.ToString();
-                        PR_TB.Text = pr.ToString();
+                        Dexterity_TB.Text = pr.ToString();
                     }
                     else
                     {
@@ -225,14 +225,14 @@ namespace GameCharacterEditor
                         Extra_TB.Text = ex.ToString();
                     }
                     break;
-                case "MR_down":
+                case "Intelligence_down":
                     int mr = int.Parse(MR_TB.Text);
                     if (mr > 20)
                     {
                         --mr;
                         ++ex;
                         Extra_TB.Text = ex.ToString();
-                        MR_TB.Text = mr.ToString();
+                        Intelligence_TB.Text = mr.ToString();
                     }
                     else
                     {
@@ -252,33 +252,33 @@ namespace GameCharacterEditor
         {
             string name;
 
-            
 
-            //if(ChooseWarriorRB.IsChecked == true)
-            //{
-            //    Warrior war = new Warrior();
-            //    name = "Warrior";
-            //    AddToDataBase(war);
-            //}
-            //else if(ChooseRogueRB.IsChecked == true)
-            //{
-            //    Rogue rogue = new Rogue();
-            //    name = "Rogue";
-            //    AddToDataBase(rogue);
-            //}
-            //else
-            //{
-            //    Sorcerer sorc = new Sorcerer();
-            //    name = "Sorcerer";
-            //    AddToDataBase(sorc);
-            //}
+
+            if (ChooseWarriorRB.IsChecked == true)
+            {
+                Warrior war = new Warrior();
+                name = "Warrior";
+                AddToDataBase(war);
+            }
+            else if (ChooseRogueRB.IsChecked == true)
+            {
+                Rogue rogue = new Rogue();
+                name = "Rogue";
+                AddToDataBase(rogue);
+            }
+            else
+            {
+                Sorcerer sorc = new Sorcerer();
+                name = "Sorcerer";
+                AddToDataBase(sorc);
+            }
 
             _todoData = new BindingList<TodoModel>()
             {
                 new TodoModel()
                 {
                     Character = name, Level = Convert.ToInt32(LevelText.Text), HP = int.Parse(HP_TB.Text), MP = int.Parse(MP_TB.Text),
-                    Strenght = int.Parse(Strenght_TB.Text), Dexterity = int.Parse(Dexterity_TB.Text), Intelligence = int.Parse(Intelligenct_TB.Text),
+                    Strenght = int.Parse(Strenght_TB.Text), Dexterity = int.Parse(Dexterity_TB.Text), Intelligence = int.Parse(Intelligence_TB.Text),
                     Constitution = int.Parse(Constitution_TB.Text), PAttack = int.Parse(PA_TB.Text), MAttack = int.Parse(MA_TB.Text),
                     PResist = int.Parse(PR_TB.Text), MResist = int.Parse(MR_TB.Text), ASpeed = int.Parse(AS_TB.Text), MSpeed = int.Parse(MS_TB.Text)
                 }
@@ -297,8 +297,8 @@ namespace GameCharacterEditor
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-             PickButton.IsEnabled = PA_down.IsEnabled = PA_up.IsEnabled = MA_down.IsEnabled = MA_up.IsEnabled =
-             PR_down.IsEnabled = PR_up.IsEnabled = MR_down.IsEnabled = MR_up.IsEnabled = false;
+             PickButton.IsEnabled = Strenght_down.IsEnabled = Strenght_up.IsEnabled = Constitution_down.IsEnabled = Constitution_up.IsEnabled =
+             Dexterity_down.IsEnabled = Dexterity_up.IsEnabled = Intelligence_down.IsEnabled = Intelligence_up.IsEnabled = false;
         }
 
         private void ExperienceButton_Click(object sender, RoutedEventArgs e)
