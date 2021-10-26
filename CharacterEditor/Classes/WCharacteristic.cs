591ef014321e91360ee336a3451e4933bb5c0f74
Гизетдинов Искандер <gizetiskander@gmail.com>
2021-10-26 08:58:11 +0300

    i add monogodb

--- CharacterEditor/Classes/WCharacteristic.cs
+++ CharacterEditor/Classes/WCharacteristic.cs
@@ -8,6 +8,34 @@ namespace CharacterEditor.Classes
 {
     class WCharacteristic
     {
+        public int WStrength { get; set; }
+        public int WAgility { get; set; }
+        public int WIntelligence { get; set; }
 
+        public void Warrior(int Wstrength, int Wagility, int Wintelligence)
+        {
+            WStrength = Wstrength;
+            WAgility = Wagility;
+            WIntelligence = Wintelligence;
+        }
+
+        public virtual void Info()
+        {
+            Console.WriteLine("Warrior Strange" + WStrength);
+            Console.WriteLine("Warrior Agility" + WAgility);
+            Console.WriteLine("Warrior Intelligence" + WIntelligence);
+        }
+
+        public virtual void WCreate(WCharacteristic wCharacteristic)
+        {
+            if(wCharacteristic.WStrength + wCharacteristic.WAgility < wCharacteristic.WIntelligence)
+            {
+                wCharacteristic.WStrength = 30;
+                wCharacteristic.WAgility = 20;
+                wCharacteristic.WIntelligence = 10;
+                Console.WriteLine("Warrior Create");
+                wCharacteristic.Info();
+            }
+        }
     }
 }
