5b19cc261a07fce675956d25b87c0c38cd7f40a3
булат шакиров <a2009-1@mail.ru>
2021-10-28 09:01:49 +0300

    add to mongodb parametri

--- Geroi/Class1.cs
+++ Geroi/Class1.cs
@@ -17,22 +17,35 @@ namespace Geroi
         protected double Strenght;
         protected double Agility;
         protected double Intelligence;
+        protected string Name;
+        protected string Class;
         private int v1;
         private int v2;
         private int v3;
+        private string v4;
+        private string v5;
 
-        public Unit(int v1, int v2, int v3)
+        public Unit(int v1, int v2, int v3, string v4, string v5)
         {
             this.v1 = v1;
             this.v2 = v2;
             this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
         }
 
-        public double strenght { get => v1; set => Strenght = value; }
+        public string _class { get => v5; set => Class = value; }
         [BsonElement]
-        public double agility { get => v2; set => Agility = value; }
+        public string _name { get => v4; set => Name = value; }
         [BsonElement]
-        public double intelligence { get => v3; set => Intelligence = value; }
+        public double _strenght { get => v1; set => Strenght = value; }
+        [BsonElement]
+        public double _agility { get => v2; set => Agility = value; }
+        [BsonElement]
+        public double _intelligence { get => v3; set => Intelligence = value; }
+        
+        
+        
 
         public void Add(Unit unit)
         {
