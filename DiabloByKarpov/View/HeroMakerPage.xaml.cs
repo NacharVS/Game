54bfc7b72431d4dd07a378d685eace3bda41dda0
Koi Den <prostobydet@gmail.com>
2021-11-02 15:07:40 +0300

    Menu stats wrok now!

--- DiabloByKarpov/View/HeroMakerPage.xaml.cs
+++ DiabloByKarpov/View/HeroMakerPage.xaml.cs
@@ -24,5 +24,15 @@ namespace DiabloByKarpov.View
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
